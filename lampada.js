30476898e7e75618392f06ddcb1eae1dc3a2cc73
LUAN ENZO SIMONATO FERRASIN <21193394@CFP127ALUNO.LOCAL>
2021-08-31 15:37:00 -0300

    Finalizado

---
+++ lampada.js
@@ -0,0 +1,28 @@
+"use strict"
+const lampada = document.getElementById("lampada")
+
+function lampadaInteira(){
+    return lampada.src.includes("ligada")
+}
+
+
+function ligarLampada(){
+    if(lampadaInteira()){
+    lampada.src = "img/ligada.jpg"
+    }
+}
+function desligarLampada(){
+    if(lampadaInteira()){
+    lampada.src = "img/desligada.jpg"
+    }
+}
+function quebrarLampada(){
+    lampada.src = "img/quebrada.jpg"
+}
+//eventos
+
+document.getElementById("ligar").addEventListener("click", ligarLampada)
+document.getElementById("desligar").addEventListener("click", desligarLampada)
+lampada.addEventListener("dblclick", quebrarLampada)
+document.getElementById("lampada").addEventListener("mouseover", ligarLampada)
+document.getElementById("lampada").addEventListener("mouseout", desligarLampada)
