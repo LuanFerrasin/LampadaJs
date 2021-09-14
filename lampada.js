eeb3e217c69c8b8909f994d15536b16214cf8b95
LUAN ENZO SIMONATO FERRASIN <21193394@CFP127ALUNO.LOCAL>
2021-09-14 13:16:13 -0300

    finalizado

--- lampada.js
+++ lampada.js
@@ -1,5 +1,7 @@
 "use strict"
 const lampada = document.getElementById("lampada")
+let idDesligar
+let idLigar
 
 function lampadaInteira(){
     return lampada.src.includes("ligada")
@@ -19,6 +21,24 @@ function desligarLampada(){
 function quebrarLampada(){
     lampada.src = "img/quebrada.jpg"
 }
+function pararPiscar(){
+   clearInterval(idLigar)
+   clearInterval(idDesligar)
+}
+function piscarLampada(){
+    const botaoPiscar = document.getElementById("piscar")
+    if (botaoPiscar.textContent == "Piscar") {
+    idDesligar = setInterval(ligarLampada, 500)
+    idLigar = setInterval(desligarLampada, 750)
+    botaoPiscar.textContent = "Parar"
+    
+    }else{
+        pararPiscar()
+        botaoPiscar.textContent = "Piscar"
+    }
+}
+
+ 
 //eventos
 
 document.getElementById("ligar").addEventListener("click", ligarLampada)
@@ -26,3 +46,5 @@ document.getElementById("desligar").addEventListener("click", desligarLampada)
 lampada.addEventListener("dblclick", quebrarLampada)
 document.getElementById("lampada").addEventListener("mouseover", ligarLampada)
 document.getElementById("lampada").addEventListener("mouseout", desligarLampada)
+document.getElementById("piscar").addEventListener("click", piscarLampada)
+
